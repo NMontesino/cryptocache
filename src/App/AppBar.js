f698af45eb99d8f1f7ae7ce53ce6d5462f6dcfe0
Nick Montesino <namontesino@gmail.com>
2021-03-24 04:40:20 -0400

    Style AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,17 +1,48 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+
+const Logo = styled.div`
+    font-size: 1.5em;
+`
 
 const Bar = styled.div`
     display: grid;
+    margin-bottom: 40px;
     grid-template-columns: 180px auto 100px 100px;
 `
 
+const ControlButtonElement = styled.div`
+    cursor: pointer;
+    ${props => props.active && css`
+        text-shadow: 0px 0px 60px #03ff03;
+    `}
+`
+
+
+const toProperCase = (lower) =>
+{
+    return `${lower.charAt(0).toUpperCase()}${lower.substr(1)}`
+}
+
+const ControlButton = ({name, active}) =>
+{
+    return (
+        <ControlButtonElement active={ active }>
+            {toProperCase(name)}
+        </ControlButtonElement>
+    )
+}
+
+
 export default () => 
 {
-    return <Bar>
-        <div>Crypto Cache</div>
-        <div/>
-        <div>Dashboard</div>
-        <div>Settings</div>
-    </Bar>
+    return (
+        <Bar>
+            <Logo>Crypto Cache</Logo>
+            <div/>
+            <ControlButton active name="dashboard"/>
+            <ControlButton name="settings"/>
+        </Bar>
+    )
 }
